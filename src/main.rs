4ad65cf6715687478a7cf5facbf38c7477488e79
Marcio Silva <marcionps@proton.me>
2023-03-14 20:38:51 -0300

    added initial structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rusty_engine::prelude::*;
+
+struct GameState {}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    game.add_logic(game_logic);
+    game.run(GameState {});
 }
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {}
