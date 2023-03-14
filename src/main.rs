0b14435cec202e1fdbe6ec2a5680b1d8830fb6e8
Marcio Silva <marcionps@proton.me>
2023-03-14 20:42:51 -0300

    updated game state

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use rusty_engine::prelude::*;
 
-struct GameState {}
+struct GameState {
+    health_amount: u8,
+    lost: bool,
+}
 
 fn main() {
     let mut game = Game::new();
 
     game.add_logic(game_logic);
-    game.run(GameState {});
+    game.run(GameState {
+        health_amount: 5,
+        lost: false,
+    });
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {}
