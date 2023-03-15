67eeb9677183cd8feeaad2d66c2419b74698fd51
Marcio Silva <marcionps@proton.me>
2023-03-15 19:24:59 -0300

    added obstacles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use rusty_engine::prelude::{bevy::utils::tracing::metadata::ParseLevelError, *};
 
 struct GameState {
@@ -10,12 +11,26 @@ const ROAD_SPEED: f32 = 400.0;
 
 fn main() {
     let mut game = Game::new();
+    let obstacle_presets = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+    ];
 
     // player sprite
     let mut player = game.add_sprite("player1", SpritePreset::RacingCarBlue);
     player.translation.x = -500.0;
     player.layer = 10.0;
     player.collision = true;
+
+    // obstacles
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
+        obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
+    }
 
     // background music
     game.audio_manager
@@ -60,6 +75,14 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
 
             if sprite.translation.x < -675.0 {
                 sprite.translation.x += 1500.00;
+            }
+        }
+
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
             }
         }
     }
